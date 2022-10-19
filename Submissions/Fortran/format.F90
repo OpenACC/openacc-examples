f6f1b5392627f49727e5dbd18c392f9af799023b
Daniel Horta <dchorta@skywalker.crpl.cis.udel.edu>
2022-10-19 13:47:31 +0000

    added Fortran folder and format file

---
+++ Submissions/Fortran/format.F90
@@ -0,0 +1,9 @@
+!       Name: Daniel Horta
+!       Email: dchorta@udel.edu
+!	GitHub Username: DandaMan65
+!	Date Code Written: 10/19/2022
+!	Features Referenced: N/A
+!	OpenACC Version: N/A
+!	Compiler(s) Used: N/A
+!
+!       Please format the header of your file like this.
