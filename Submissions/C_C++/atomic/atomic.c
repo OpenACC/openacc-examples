e49167f8fb99f1ca2914b2a746d9d4d59cf21c2d
Aaron Liu <olympus@skywalker.crpl.cis.udel.edu>
2023-04-10 05:32:08 +0000

    this is the atomic construct example, the atomic capture has a bug

---
+++ Submissions/C_C++/atomic/atomic.c
@@ -0,0 +1,59 @@
+
+/*1  <---------------->
+2   Your Name: Aaron Liu
+3   Email: Olympus@ude.edu
+4   GitHub Username: AaronLiu20
+5   Date Code Written: 10/17/2022
+6   Features Referenced: atomic construct with update, write, capture
+7   OpenACC Version: 3.3
+8   Compiler(s) Used: Nvidia
+9
+10 this is a example of the atomic construct that prevents multiple reads and writes on a variable
+11  <----------------> */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n = 100;
+    double * data = (double *)malloc( n * sizeof(double));
+    for( int x = 0; x < n; ++x){
+	    data[x] = x;	    
+    }
+
+    double writeSum = 0.0;
+    double captureSum = 0.0;
+    double updateSum = 0.0;
+
+    // the atomic construct prevents reading the value while a gang/worker/vector is writing and vice versa
+    //this is the write clause that only allows a direct write to a variable from a expression
+    #pragma acc parallel loop copy(data[0:n]) copyout(writeSum)
+    for(int x = 0; x < n; ++x){
+        if(data[x] >= n/2){
+            #pragma acc atomic write
+            writeSum = x;
+        }
+    }
+
+    //this is the capture clause that the update to the expression into another variable
+    #pragma acc parallel loop copy(data[0:n]) copyout(captureSum)
+    for(int x = 0; x < n; ++x){
+        if(data[x] >= n/2){
+            #pragma acc atomic capture
+            captureSum = data[x]--;
+            //printf("%f. %f. %d\n", captureSum, data[x], x);
+        }
+    }
+
+    printf("%f\n", captureSum);
+
+    //this is the update clause which locks the update of a particualar variable from certain operations
+    #pragma acc parallel loop copy(data[0:n]) copyout(updateSum)
+    for(int x = 0; x < n; ++x){
+        if(data[x] >= n/2){
+            #pragma acc atomic update
+            updateSum++;
+        }
+    }
+    return 0;
+}
